167f44b61a84717d4a449612beccfa1dcf8fa33e
Hadia Iftikhar <55398310+HadiaIftikhar-HIi@users.noreply.github.com>
2019-10-08 21:54:56 +0500

    Add files via upload

---
+++ str1 and str2 together - Copy.vb
@@ -0,0 +1,25 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim str1, str2, firstword, secword As String
+        Dim sp As Integer
+        str1 = ""
+        str2 = ""
+        firstword = ""
+        secword = ""
+        sp = 0
+        Console.Write("enter first string: ")
+        str1 = Console.ReadLine
+        sp = InStr(str1, " ")
+        firstword = Left(str1, sp - 1)
+        Console.Write("enter second string: ")
+        str2 = Console.ReadLine
+        sp = InStr(str2, " ")
+        secword = Right(str2, Len(str2) - sp)
+        Console.WriteLine("final string is: " & firstword & " " & secword)
+
+        Console.ReadKey()
+
+    End Sub
+
+End Module
