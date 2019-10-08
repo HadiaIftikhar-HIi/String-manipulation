45ad659f1bea4b6bb0f6060c970cde15a7a1cf81
Hadia Iftikhar <55398310+HadiaIftikhar-HIi@users.noreply.github.com>
2019-10-08 22:09:43 +0500

    Add files via upload

---
+++ inverse - Copy.vb
@@ -0,0 +1,20 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim str1, str2, nextchar As String
+        Dim count As Integer
+        str1 = ""
+        str2 = ""
+        count = 0
+        Console.Write("enter string to inverse : ")
+        str1 = Console.ReadLine
+        For count = Len(str1) To 1 Step -1
+            nextchar = Mid(str1, count, 1)
+            str2 = str2 + nextchar
+        Next
+
+        Console.WriteLine(" inverse string is: " & str2)
+        Console.ReadKey()
+    End Sub
+
+End Module
